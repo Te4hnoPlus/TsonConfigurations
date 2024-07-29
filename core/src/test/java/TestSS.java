b8cb2c71c934ce574f2f4e9e8324701efe99afd0
Te4hnoPlus <te4hnopublic@yandex.ru>
2024-07-29 18:19:21 +0500

    speedy / fix

--- core/src/test/java/TestSS.java
+++ core/src/test/java/TestSS.java
@@ -1,35 +1,35 @@
 
-import plus.tson.TsonMap;
-import plus.tson.security.ClassManager;
+//import plus.tson.TsonMap;
+//import plus.tson.security.ClassManager;
 
 public class TestSS {
 
     public static void main(String[] args) {
-        TsonMap map = new TsonMap(new ClassManager.Empty(), """
-{
-    structures=[
-        <(normal), {
-            id=(1),
-            chance=(0.5),
-            biomes=["plains"],
-            spawn-at=["grass"],
-            structure="test",
-            max-pow=(50),
-            pos=[(0),(1),(0)],
-            add=[(0)],
-            settings=[
-                ["rot-90"],
-                ["struct-block"],
-                ["entities", "m-left", "m-front", "ignore:air"]
-            ]
-        }>
-    ],
-
-    classes=[(normal), (sub-simple), (w), (w+), (multi), (sub-multi), (choose)]
-}
-                """);
-
-        System.out.println(map);
+//        TsonMap map = new TsonMap(new ClassManager.Empty(), """
+//{
+//    structures=[
+//        <(normal), {
+//            id=(1),
+//            chance=(0.5),
+//            biomes=["plains"],
+//            spawn-at=["grass"],
+//            structure="test",
+//            max-pow=(50),
+//            pos=[(0),(1),(0)],
+//            add=[(0)],
+//            settings=[
+//                ["rot-90"],
+//                ["struct-block"],
+//                ["entities", "m-left", "m-front", "ignore:air"]
+//            ]
+//        }>
+//    ],
+//
+//    classes=[(normal), (sub-simple), (w), (w+), (multi), (sub-multi), (choose)]
+//}
+//                """);
+//
+//        System.out.println(map);
 
         //TsonFile file = new TsonFile("solar-repo.tson");
         //System.out.println(file);
